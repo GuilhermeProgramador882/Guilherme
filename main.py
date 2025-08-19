281c7ed054468333c95bf08020874853c1bae7f3
Guilherme <golivaandrade@gmail.com>
2025-08-19 20:55:49 -0300

    desafio

---
+++ main.py
@@ -0,0 +1,5 @@
+numero = 5
+
+for num in range(1, 1001):
+    print("o numero",numero,"vezes",num,"e igual",num*numero)
+
---
+++ main2.py
@@ -0,0 +1,7 @@
+numero = -1
+if numero < 0:
+    print("negativo")
+elif numero == 0:
+    print("zero")
+elif numero > 0:
+    print("positivo")

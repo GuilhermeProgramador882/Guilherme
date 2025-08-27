db70a5df30e33ea2267cf76d1a7925d0dfcd0686
GuilhermeProgramador882 <golivaandrade@gmail.com>
2025-08-27 17:30:35 -0300

    Atividade

--- numeros_primos.py
+++ numeros_primos.py
@@ -1,7 +1,7 @@
-for num in range(1, 51):  # percorre de 1 até 50
-    if num > 1:  # números primos são maiores que 1
+for num in range(1, 51):  
+    if num > 1:  
         eh_primo = True
-        for i in range(2, num):  # testa divisores de 2 até num-1
+        for i in range(2, num):  
             if num % i == 0:
                 eh_primo = False
                 break

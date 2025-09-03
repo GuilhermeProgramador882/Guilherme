6e8af043fe10b6f197c22572f457fb17f9540ef5
Guilherme <golivaandrade@gmail.com>
2025-09-02 21:01:21 -0300

    lista

---
+++ lista.py
@@ -0,0 +1,11 @@
+#lista = ["Maçã","Picanha","Mercedes","Jato particular"]
+#lista.append("Mansão")
+#lista.remove("Maçã")
+#print(lista)
+#print(len(lista))
+
+numeros = (5,10,15)
+texto = " ".join (map(str,numeros))
+print(texto)
+lista_denovo = texto.split(" ")
+print(lista_denovo)

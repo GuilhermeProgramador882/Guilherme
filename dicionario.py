cc8a5ec646e6aaa189cfac967dbf4c0c0429e795
Guilherme <golivaandrade@gmail.com>
2025-09-04 20:44:58 -0300

    dicionario

---
+++ dicionario.py
@@ -0,0 +1,13 @@
+aluno = {
+    "nome" : "Ana",
+    "idade" : "22",
+    "curso" :"Jogo de formigas jogando baseball",
+}
+aluno["altura"] = 9.5
+aluno ["curso"] = "Programação"
+print(aluno)
+print(aluno.get("compras", "Errado"))
+
+
+
+

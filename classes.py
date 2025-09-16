0f3a6a4cf031cf2ef9f2fc26326e776a3dd7e586
Guilherme <golivaandrade@gmail.com>
2025-09-16 20:02:59 -0300

    classesv2

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def apresentar(self):
+        return f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos"
+    
+    def envelhecer(self, anos):
+        self.idade += anos
+        return f"{self.nome} agora tem {self.idade} anos"
